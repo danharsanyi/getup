ed3861dc64592dd5e686ba2fa90c2f0668639618
danharsanyi <dan@conqr.co>
2016-08-01 09:49:04 +1000

    Initial Commit

---
+++ getup/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  getup
+//
+//  Created by Dan Harsanyi on 1/8/16.
+//  Copyright © 2016 danharsanyi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
